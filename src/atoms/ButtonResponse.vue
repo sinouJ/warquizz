44a6f944abfcdf28a3fccb4fdb9441cdbe5e4597
Jordan Sinou <sinoujordan@gmail.com>
2023-01-31 14:05:02 +0100

    init

---
+++ src/atoms/ButtonResponse.vue
@@ -0,0 +1,21 @@
+<template>
+    <button>{{ response }}</button>
+</template>
+
+<script>
+export default {
+    name: "ButtonResponse",
+    props: {
+        response: String
+    }
+};
+
+</script>
+
+<style lang="scss" scoped>
+@import '../sass/style.scss';
+
+    button {
+        background-color: $yellow;
+    }
+</style>

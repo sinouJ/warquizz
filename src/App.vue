575768be05bf783b230c4df982625ec622c58ed4
Jordan Sinou <sinoujordan@gmail.com>
2023-01-31 16:09:25 +0100

    button

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
-<script setup>
+<script>
+import ButtonResponse from "./atoms/ButtonResponse.vue";
+
+export default {
+  components: {
+    ButtonResponse,
+  },
+};
 </script>
 
 <template>
+  <div class="container">
+    <button-response index="A" response="Îles Malouines" />
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+@import "./sass/style.scss";
+
+#app {
+  height: 100vh;
+  background-color: $background;
+}
 </style>
--- src/atoms/ButtonResponse.vue
+++ src/atoms/ButtonResponse.vue
@@ -1,21 +1,40 @@
 <template>
-    <button>{{ response }}</button>
+  <button>
+    <p class="semibold">{{ index }}.</p>
+    <p>{{ response }}</p>
+  </button>
 </template>
 
 <script>
 export default {
-    name: "ButtonResponse",
-    props: {
-        response: String
-    }
+  name: "ButtonResponse",
+  props: {
+    response: String,
+    index: String,
+  },
 };
-
 </script>
 
 <style lang="scss" scoped>
-@import '../sass/style.scss';
+@import "../sass/style.scss";
 
-    button {
-        background-color: $yellow;
+button {
+  font-family: "Cabin", sans-serif;
+  display: block;
+  width: 100%;
+  background-color: $primary;
+  padding: 20px 20px;
+  font-size: 16px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  border-radius: $radius;
+
+  p {
+    &:last-child {
+      margin-left: 10px;
     }
+  }
+}
 </style>
